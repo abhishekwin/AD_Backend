ef7e0185e397c1e99164acac09306df8ce3f8450
satish <satish@menttechlabs.com>
2022-11-08 11:31:12 +0530

    work on collection or nft

--- cron/launchpad/launchPadCreateNftsUsingCollection.js
+++ cron/launchpad/launchPadCreateNftsUsingCollection.js
@@ -24,70 +24,78 @@ const getBaseWebDataUsingAxios = async (url, count = 0) => {
       resolve(response.data)
     })
       .catch(function (error) {
-        resolve(null)    
+        resolve(null)
       })
   });
   return promise;
 };
 
+
+const createNftWithUri = async (id, updateUri, data, failedNfts) => {
+  await new Promise(async function (resolve, reject) {
+    let baseResponse = await getBaseWebDataUsingAxios(updateUri);
+    if (baseResponse == null) {
+      await sleep(3000)
+      baseResponse = await getBaseWebDataUsingAxios(updateUri);
+    }
+    if (baseResponse == null) {
+      await sleep(3000)
+      baseResponse = await getBaseWebDataUsingAxios(updateUri);
+    }
+
+    if (baseResponse) {
+      let objNfts = {
+        collectionId: data._id,
+        collectionAddress: data.collectionAddress,
+        royalties: data.royalties ? data.royalties : 0,
+        name: baseResponse.name,
+        description: baseResponse.description,
+        image: baseResponse.image,
+        tokenURI: updateUri ? updateUri : null,
+        owner: data.creator,
+        creator: data.creator,
+        tokenId: id,
+        // dna: baseResponse.dna,
+        attributes: baseResponse.attributes,
+        compiler: baseResponse.compiler,
+        currency: data.currency,
+        isFirstSale: true,
+        mintCost: data.mintCost,
+        royalties: data.royalties,
+        status: "Active",
+        isActive: true,
+        networkId: data.networkId,
+        networkName: data.networkName,
+      };
+
+      let existNfts = await LaunchPadNft.findOne({
+        collectionId: data._id,
+        tokenId: id
+      });
+
+      if (!existNfts) {
+        await LaunchPadNft.create(objNfts)
+      }
+
+    } else {
+      failedNfts.push(id)
+    }
+    resolve(failedNfts)
+  });
+  return failedNfts;
+};
+
 const createNftUsingCollectionFuncation = async () => {
   const data = await LaunchPadCollection.findOne({ status: "ready-to-syncup" });
+
   if (data) {
     let failedNfts = [];
     await LaunchPadCollection.findOneAndUpdate({ _id: data._id }, { status: "syncing" })
     for (let step = 1; step <= data.maxSupply; step++) {
       const id = step
       updateUri = data.tokenURI + id + ".json";
-     
-      let baseResponse = await getBaseWebDataUsingAxios(updateUri);
-      if(baseResponse == null){
-        await sleep(3000)
-        baseResponse = await getBaseWebDataUsingAxios(updateUri);
-      }
-      if(baseResponse == null){
-        await sleep(3000)
-        baseResponse = await getBaseWebDataUsingAxios(updateUri);
-      }
-      
-      if (baseResponse) {
-        let objNfts = {
-          collectionId: data._id,
-          collectionAddress: data.collectionAddress,
-          royalties: data.royalties ? data.royalties : 0,
-          name: baseResponse.name,
-          description: baseResponse.description,
-          image: baseResponse.image,
-          tokenURI: updateUri ? updateUri : null,
-          owner: data.creator,
-          creator: data.creator,
-          tokenId: id,
-          // dna: baseResponse.dna,
-          attributes: baseResponse.attributes,
-          compiler: baseResponse.compiler,
-          currency: data.currency,
-          isFirstSale: true,
-          mintCost: data.mintCost,
-          royalties: data.royalties,
-          status: "Active",
-          isActive: true,
-          networkId: data.networkId,
-          networkName: data.networkName,
-        };
-
-        let existNfts = await LaunchPadNft.findOne({
-          collectionId: data._id,
-          tokenId: id
-        });
-
-        if (!existNfts) {
-          await LaunchPadNft.create(objNfts)
-        } 
-
-      }else{
-        failedNfts.push(id)
-      }
+      let nftCreated = await createNftWithUri(id, updateUri, data, failedNfts);
     }
-    // console.log("Done")
     await LaunchPadCollection.findOneAndUpdate({ _id: data._id }, { status: "completed", failedNfts: failedNfts })
   }
 
@@ -96,5 +104,5 @@ const createNftUsingCollectionFuncation = async () => {
 // createNftUsingCollectionFuncation();
 
 module.exports = {
-    createNftUsingCollectionFuncation
+  createNftUsingCollectionFuncation
 };
