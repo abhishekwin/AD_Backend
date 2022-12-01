ac13ce020a1651485b6674bab3c2faad30b5b6c7
satish <satish@menttechlabs.com>
2022-12-01 21:45:41 +0530

    work on cron

--- app.js
+++ app.js
@@ -103,12 +103,12 @@ cron.schedule('*/10 * * * * *', () => {
 });
 
 
-// cron.schedule(`*/${process.env.DB_BACKUP_START} * * *`, () => {
-//   console.log("---dump cron---")
-//   if(process.env.DB_BACKUP_START){
-//     dumpDataBase()
-//   }  
-// });
+cron.schedule(`0 */${process.env.DB_BACKUP_START} * * *`, () => {
+  console.log("---dump cron---")
+  if(process.env.DB_BACKUP_START){
+    dumpDataBase()
+  }  
+});
 
 
 app.use(Sentry.Handlers.errorHandler());
